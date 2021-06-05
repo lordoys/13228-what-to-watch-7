2251dfc9a0425489507be87b790face03a3a0116
maksimpecerin <lordoys@gmail.com>
2021-06-05 14:48:11 +0300

    Добавил роутинг страниц

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import MainPage from '../mainPage/mainPage';
+import SignInPage from '../signInPage/signInPage';
+import MyListPage from '../myListPage/myListPage';
+import FilmPage from '../filmPage/filmPage';
+import AddReviewPage from '../addReviewPage/addReviewPage';
+import PlayerPage from '../playerPage/playerPage';
 import PropTypes from 'prop-types';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 function App({genre, year}) {
   return (
-    <MainPage genre={genre} year={year} />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MainPage genre={genre} year={year} />
+        </Route>
+        <Route path="/login" exact component={SignInPage} />
+        <Route path="/mylist" exact component={MyListPage} />
+        <Route path="/films/:id" exact component={FilmPage} />
+        <Route path="/films/:id/review" exact component={AddReviewPage} />
+        <Route path="/player/:id" exact component={PlayerPage} />
+        <Route
+          render={() => (
+            <>
+              <h1>
+                404.
+                <br />
+                <small>Page not found</small>
+              </h1>
+              <Link to="/">Go to main page</Link>
+            </>
+          )}
+        />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/components/notFoundPage/notFoundPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFoundPage() {
+  return (
+    <h1>
+      404.
+      <br />
+      <small>Page not found</small>
+    </h1>
+  );
+}
+
+export default NotFoundPage;
