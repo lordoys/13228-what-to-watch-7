fa5d55dfdc708aa13ef353cd62222fc6024fcc09
Pecherin Maxim <lordoys@gmail.com>
2021-06-23 23:14:45 +0300

    Добавил логику вывода похожих фильмов

--- project/src/components/film-page/film-page.jsx
+++ project/src/components/film-page/film-page.jsx
@@ -4,6 +4,7 @@ import filmsProp from '../../props/films.prop';
 import {AppRoute} from '../../routes';
 import {Link} from 'react-router-dom';
 import FilmTabs from '../film-tabs/film-tabs';
+import FilmList from '../film-list/film-list';
 
 function FilmPage({films, match}) {
   const id = match.params.id;
@@ -81,43 +82,7 @@ function FilmPage({films, match}) {
         <section className="catalog catalog--like-this">
           <h2 className="catalog__title">More like this</h2>
 
-          <div className="catalog__films-list">
-            <article className="small-film-card catalog__films-card">
-              <div className="small-film-card__image">
-                <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175"/>
-              </div>
-              <h3 className="small-film-card__title">
-                <Link className="small-film-card__link" to={`${AppRoute.FILM_BASE_IRL}/${film.id}/overview`}>{film.name}</Link>
-              </h3>
-            </article>
-
-            <article className="small-film-card catalog__films-card">
-              <div className="small-film-card__image">
-                <img src="img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175"/>
-              </div>
-              <h3 className="small-film-card__title">
-                <Link className="small-film-card__link" to={`${AppRoute.FILM_BASE_IRL}/${film.id}/overview`}>{film.name}</Link>
-              </h3>
-            </article>
-
-            <article className="small-film-card catalog__films-card">
-              <div className="small-film-card__image">
-                <img src="img/macbeth.jpg" alt="Macbeth" width="280" height="175"/>
-              </div>
-              <h3 className="small-film-card__title">
-                <Link className="small-film-card__link" to={`${AppRoute.FILM_BASE_IRL}/${film.id}/overview`}>{film.name}</Link>
-              </h3>
-            </article>
-
-            <article className="small-film-card catalog__films-card">
-              <div className="small-film-card__image">
-                <img src="img/aviator.jpg" alt="Aviator" width="280" height="175"/>
-              </div>
-              <h3 className="small-film-card__title">
-                <Link className="small-film-card__link" to={`${AppRoute.FILM_BASE_IRL}/${film.id}/overview`}>{film.name}</Link>
-              </h3>
-            </article>
-          </div>
+          <FilmList films={films} genre={film.genre}/>
         </section>
 
         <footer className="page-footer">
