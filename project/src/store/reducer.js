47406e90d104fa3c7690d76895ade63d94deb88c
maksimpecerin <lordoys@gmail.com>
2021-06-28 12:16:32 +0300

    Добавил начальное состояние стейта и функцию редьюсер

---
+++ project/src/store/reducer.js
@@ -0,0 +1,21 @@
+const initialState = {
+  genre: '',
+  films: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case '':
+      return {
+
+      };
+    case '':
+      return {
+
+      };
+    default:
+      return state;
+  }
+};
+
+export {reducer};
