a4d6faec40338d156a2f144a78e6898b3f69ba4d
Pecherin Maxim <lordoys@gmail.com>
2021-06-29 23:07:35 +0300

    Вынес рендер 2-х одинаковых блоков в функцию

--- project/src/components/film-reviews/film-reviews.jsx
+++ project/src/components/film-reviews/film-reviews.jsx
@@ -15,35 +15,28 @@ function FilmReviews({reviews, film}) {
     return getReviewsForCurrentFilm(list).filter((e,i)=>(i%2));
   }
 
+  function renderReview(review, index) {
+    return (
+      <div key={String(index)} className="review">
+        <blockquote className="review__quote">
+          <p className="review__text">{review.comment}</p>
+          <footer className="review__details">
+            <cite className="review__author">{review.user}</cite>
+            <time className="review__date" dateTime="2016-12-24">{review.date}</time>
+          </footer>
+        </blockquote>
+        <div className="review__rating">{review.rating}</div>
+      </div>
+    );
+  }
+
   return (
     <div className="film-card__reviews film-card__row">
       <div className="film-card__reviews-col">
-        {getEven(reviews).map((review, index) => (
-          <div key={String(index)} className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">{review.comment}</p>
-              <footer className="review__details">
-                <cite className="review__author">{review.user}</cite>
-                <time className="review__date" dateTime="2016-12-24">{review.date}</time>
-              </footer>
-            </blockquote>
-            <div className="review__rating">{review.rating}</div>
-          </div>
-        ))}
+        {getEven(reviews).map((review, index) => renderReview(review, index))}
       </div>
       <div className="film-card__reviews-col">
-        {getOdd(reviews).map((review, index) => (
-          <div key={String(index)} className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">{review.comment}</p>
-              <footer className="review__details">
-                <cite className="review__author">{review.user}</cite>
-                <time className="review__date" dateTime="2016-12-24">{review.date}</time>
-              </footer>
-            </blockquote>
-            <div className="review__rating">{review.rating}</div>
-          </div>
-        ))}
+        {getOdd(reviews).map((review, index) => renderReview(review, index))}
       </div>
     </div>
   );
