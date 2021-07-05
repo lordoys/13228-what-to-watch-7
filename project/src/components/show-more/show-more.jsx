839532b76f1db5c6c072c405716acb7d2a9503dd
maksimpecerin <lordoys@gmail.com>
2021-07-05 09:52:32 +0300

    Убрал лишнюю фенкцию

--- project/src/components/show-more/show-more.jsx
+++ project/src/components/show-more/show-more.jsx
@@ -4,13 +4,9 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
 function ShowMore({increaseCount}) {
-  function showMore() {
-    increaseCount();
-  }
-
   return (
     <div className="catalog__more">
-      <button onClick={showMore} className="catalog__button" type="button">Show more</button>
+      <button onClick={increaseCount} className="catalog__button" type="button">Show more</button>
     </div>
   );
 }
